9584377c0968ac82371827f1fed9097ea8b5d9c2
Ayobami <oyebisiayobami26@gmail.com>
2026-09-24 00:15:40 +0100

    feat: add typed feature flags

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validate } from './config/env.validation';
+import { FeatureFlagsModule } from './config/feature-flags.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -15,6 +16,7 @@ import { NotificationsModule } from './notifications/notifications.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate }),
+    FeatureFlagsModule,
     PrismaModule,
     StellarModule,
     AuthModule,
